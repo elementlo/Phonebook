e6efc5417979ca7a582e21479feb9b3f3b6cab7a
huaxizhang <zhangxisd@vip.qq.com>
2017-06-27 18:32:15 +0800

    add firebase

--- app/src/main/java/com/weikuo/elemenzhang/phonebookwk/MyApplication.java
+++ app/src/main/java/com/weikuo/elemenzhang/phonebookwk/MyApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
 import com.github.tamir7.contacts.Contacts;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
 
@@ -14,11 +15,17 @@ import io.fabric.sdk.android.Fabric;
  */
 
 public class MyApplication extends Application {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
     @Override
     public void onCreate() {
         super.onCreate();
         Contacts.initialize(this);
         Logger.addLogAdapter(new AndroidLogAdapter());
         Fabric.with(this, new Crashlytics());
+
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
     }
 }
